6a8f4fbfd0f4acd91b3f19351c5caf8b34c39200
mdfaizanashrafi <mdfaizanashrafi13032001@gmail.com>
2025-04-17 13:17:01 +0530

    more

---
+++ models1.py
@@ -0,0 +1,8 @@
+#Linear Regression using Seoul Bike Sharing Demand
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from imblearn.over_sampling import RandomOverSampler
+from sklearn.preprocessing import StandardScaler
+
