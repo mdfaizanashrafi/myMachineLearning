08453879feddfe04da7eb1be78e5df0c5e5f5a60
mdfaizanashrafi <mdfaizanashrafi13032001@gmail.com>
2025-04-19 20:12:25 +0530

    more NN

--- models1.py
+++ models1.py
@@ -194,3 +194,72 @@ history = nn_model.fit(
     validation_data=(x_val_all,y_val_all),
     verbose=0,epochs=100)
 
+#=====================================================================
+# UNSUPERVISED lEARNING: HAVING BUNCH OF UNLABELLED DATA
+#=================================================================
+
+#k-Clustering:
+
+import seaborn as sns
+
+cols = ["area","perimeter","compactness","length","width","asymmetry","groove","class"]
+df = pd.read_csv("seeds_dataset.txt", names=cols)
+
+for i in range(len(cols)-1):
+    for j in range(i+1, len(cols)-1):
+        x_label = cols[i]
+        y_label = cols[j]
+        sns.scatterplot(x=x_label, y=y_label, data= df, hue='class')
+
+from sklearn.cluster import KMeans
+
+x= "perimeter"
+y= "asymmetry"
+z= df[[x,y]].values
+
+kmeans = KMeans(n_clusters=3).fit(x)
+clusters = kmeans.labels_
+clusters
+
+df["class"].values
+
+cluster_df = pd.DataFrame(np.hstack((x,clusters.reshape(-1,1))), columns=[x,y, "class"])
+sns.scatterplot(x=x, y=y,hue="class", data=cluster_df)
+plt.plot()
+
+z = df[cols[:-1]].values
+kmeans = KMeans(n_clusters=3).fit(z)
+cluster_df = pd.DataFrame(np.hstack((z, clusters.reshape(-1,1))), columns= df.columns)
+
+sns.scatterplot(x=x , y=y, hue="class", data=cluster_df)
+
+#Original df:
+sns.scatterplot(x=x,y=y,hue="class",data=df)
+plt.plot()
+
+# PCA
+
+from sklearn.decomposition import PCA
+pca=PCA(n_components=2)
+transformed_x = pca.fit_transform(z)
+
+z.shape
+
+transformed_x.shape
+
+plt.scatter(transformed_x[:,0], transformed_x[:,1])
+plt.show()
+
+kmeans_pca_df = pd.DataFrame(np.hstack((transformed_x, kmeans.labels_.reshape(-1,1))), columns= ["pca1","pca2","class"])
+
+truth_pcs_df = pd.DataFrame(np.hstack((transformed_x, df["class"].values.reshape(-1,1))),columns=["pca1","pca2","class"])
+
+sns.scatterplot(x="pca1",y="pca2",hue="class",data=kmeans_pca_df)
+
+#Truth CLasses 
+
+sns.scatterplot(x="pca1",y="pca2",hue="class",data=truth_pcs_df)
+plt.show()
+
+
+
